Fix a couple of cases where line gradients could vanish:  - Add code to reinitialize lines that fade out of existence  - Move clipping out of renderer and add a loss that penalizes over-dark regions, to fix gradients vanishing if many lines end up on top of one another

spline_render.py
@@ -111,6 +111,26 @@ class QuadraticSplineParams(nn.Module):
                 for i in range(self.n_lines):
                     self.a[0, i, :], self.b[0, i, :], self.c[0, i, :] = init_spline_pmap(self.a.device, img_cdf, img_pdf.shape)
 
+    def reinit_invisible(self, min_intensity=0.025, init_img = None):
+        with torch.no_grad():
+
+            if init_img is not None:
+                img_pdf = compute_pdf_grads(init_img)
+                img_cdf = np.cumsum(img_pdf)
+                img_cdf = img_cdf / img_cdf[-1]
+
+            for i in range(self.n_lines):
+                if self.lc[0, i, 0].item() < min_intensity:
+                    print("reinit", i)
+
+                    if init_img is not None:
+                        self.a[0, i, :], self.b[0, i, :], self.c[0, i, :] = init_spline_pmap(self.a.device, img_cdf,
+                                                                                             img_pdf.shape)
+                    else:
+                        self.a[0, i, :], self.b[0, i, :], self.c[0, i, :] = init_spline_random(self.a.device)
+
+                    self.lc[0, i, 0] = 0.5
+
     def forward(self):
         # Return parameters directly
         return self.a, self.b, self.c, self.lw, self.lc
@@ -237,7 +257,7 @@ class QuadraticSplineRenderer(nn.Module):
         # Additive blending + clamp over all lines --> shape (rows, cols, 1)
         # TODO: Actual blend should be: blended = intensity + (1 - intensity) * prev_img
         #       That can be done in a loop.  I probably need to be reminded why doing it this way is a bad idea.
-        intensity = torch.clamp(torch.sum(intensity, dim=1), 0.0, 1.0)
+        intensity = torch.sum(intensity, dim=1)
 
         # Invert (we want dark lines on white background)
         intensity = 1 - intensity

train_spline_img_lpips.py
@@ -12,9 +12,9 @@ import numpy as np
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    img_path = "data/alyx.png"
+    img_path = "data/heeler_puppy.jpg"
     draw_sz = (256, 256)
-    n_lines = 768
+    n_lines = 600
 
     target_img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
     target_img = cv2.resize(target_img, draw_sz, interpolation=cv2.INTER_AREA)
@@ -45,15 +45,26 @@ def main():
 
     perceptual_loss = LPIPS().to(device)
 
+    best_loss = 1e9
+
     for i in range(1000000):
         start_t = time.perf_counter()
 
         line_params.zero_grad()
         img_render = lines(*line_params())
 
-        err = perceptual_loss(img_render, target[None, ...])
+        # penalize big negative intensities
+        # should allow some lines to sit atop one another, but will hurt large piles
+        err_scribble = torch.sum(torch.square(torch.clamp(img_render, min=None, max=-3) + 3))
 
+        # clip image
+        img_render = torch.clip(img_render, min=0, max=1)
+
+        err_perceptual = perceptual_loss(img_render, target[None, ...])
+
+        err = err_scribble + err_perceptual
         err.backward()
+
         optim.step()
 
         # sanity clamping
@@ -64,11 +75,18 @@ def main():
             line_params.lw.clamp_(1e-5, 0.10)
             line_params.lc.clamp_(0.0, 1.0)
 
-        img_np = np.clip(img_render.detach().cpu().numpy() * 255, 0, 255)
-        cv2.imwrite(os.path.join(out_dir, "%06d.png" % i), img_np[0, 0, ...])
+        loss = err.item()
+
+        if loss < best_loss:
+            best_loss = loss
+            img_np = np.clip(img_render.detach().cpu().numpy() * 255, 0, 255)
+            cv2.imwrite(os.path.join(out_dir, "%06d_%0.04f.png" % (i, best_loss)), img_np[0, 0, ...])
+        else:
+            # reinit invisible if we are taking negative steps
+            line_params.reinit_invisible(init_img=target_img)
 
         end_t = time.perf_counter()
-        print("Iter %d Loss %f Time %f" % (i, err.item(), end_t-start_t))
+        print("Iter %d Loss %f (p %f sc %f) Time %f Mem %f GB" % (i, err.item(), err_perceptual.item(), err_scribble.item(), end_t-start_t, torch.cuda.max_memory_allocated(device)/1024/1024/1024) + (" ***" if loss == best_loss else ""))
 
 
 if __name__ == "__main__":